9d13a73358fb99f09d4d80b1808e847b660ea3c5
saqibhaneef <muhammadsaqib.ms228@gmail.com>
2024-08-23 10:53:16 +0500

    app component renderd

--- src/Tests/App.test.jsx
+++ src/Tests/App.test.jsx
@@ -2,24 +2,25 @@
 import { it,expect, test, describe, vi } from 'vitest'
 import { screen,render } from '@testing-library/react'
 import App from '../App'
-import CommentBox from '../Components/CommentBox'
 
 describe('App Component Test', () => {    
     
-    render(<App/>)
+    const {container}=render(<App/>) 
+    screen.debug()
+    it('Should render App Component', () => {
+        expect(container).not.toBeNull();       
+    })
 
     it('Contains Comment Box', () => {
-        screen.debug()
 
         const divElements=document.querySelectorAll('div');
         
         const hasCommentBoxText = Array.from(divElements).some(div => div.textContent.includes('Comment Box'));
         
         expect(hasCommentBoxText).toBeTruthy()
-    })
+    })    
 
     it('Contains Comment List', () => {
-        screen.debug()
 
         const divElements=document.querySelectorAll('div');
         
